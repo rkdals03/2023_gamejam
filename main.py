ca45cbe6976004f574d0a7276a6fca53667fcdb0
rkdals03 <dev_cpp@kakao.com>
2023-11-17 19:42:30 +0900

    make player

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from ursina import *
+from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 
+app = Ursina()
+
+camera.orthographic = True
+camera.fov = 20
+ 
+ground = Entity(
+    model = 'cube',
+    color = color.rgb(50,180,50),
+    z=0.1,
+    y=-8,
+    scale = (100, 5, 10),
+    collider = 'box'
+)
+
+player = PlatformerController2d()
+
+app.run()
